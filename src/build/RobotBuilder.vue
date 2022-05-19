7ba8727ced949fbe10afd4089cb9ca38c3133d3d
laszlo-ratesic <keenanrc@hotmail.com>
2022-05-19 12:57:56 -0500

    mixins

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
-      <div class="top part">
+      <div :class="[saleBorderClass, 'top', 'part']">
         <div class="robot-name">
           {{ selectedRobot.head.title }}
           <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -71,6 +71,7 @@
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPrevValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -84,6 +85,7 @@ function getNextValidIndex(index, length) {
 
 export default {
   name: 'RobotBuilder',
+
   // any data you want to bind to MUST be initialized in the data function
   data() {
     return {
@@ -96,7 +98,18 @@ export default {
       selectedBaseIndex: 0,
     };
   },
+  mixins: [createdHookMixin],
   computed: {
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
+    headBorderStyle() {
+      return {
+        border: this.selectedRobot.head.onSale ?
+          '3px solid red' :
+          '3px solid grey',
+      };
+    },
     selectedRobot() {
       return {
         head: availableParts.heads[this.selectedHeadIndex],
@@ -181,12 +194,18 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .part {
   position: relative;
   width: 165px;
   height: 165px;
   border: 3px solid #aaa;
+}
+
+.part {
+  .img {
+    width: 165px;
+  }
 }
 
 .part img {
@@ -322,5 +341,9 @@ th {
 
 .cost {
   text-align: right;
+}
+
+.sale-border {
+  border: 3px solid red;
 }
 </style>
